b036ab1d5275ecabaff2dab5d6444c5ab1ed1257
SeraAres09 <seraares202009@gmail.com>
2025-01-10 11:48:55 +0300

    Update getExpenses.ts

--- pages/api/getExpenses.ts
+++ pages/api/getExpenses.ts
@@ -1,32 +1,32 @@
+import { readFromExpensesSheet } from '../../utils/googleSheets'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { readFromSheet } from '../../utils/googleSheets'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     try {
-      const expenses = await readFromSheet()
+      const expenses = await readFromExpensesSheet()
       
       // İlk satır başlıkları içerdiği için onu ayırıyoruz
       const [headers, ...data] = expenses
 
       // Filtreleme
       const filteredData = data.filter(row => {
-        const [date, place, type, , spender] = row
-        const { startDate, endDate, place: filterPlace, type: filterType, spender: filterSpender } = req.query
+        const [tarih, harcamaYeri, harcamaTuru, , harcamayiYapan] = row
+        const { baslangicTarihi, bitisTarihi, harcamaYeri: filterHarcamaYeri, harcamaTuru: filterHarcamaTuru, harcamayiYapan: filterHarcamayiYapan } = req.query
 
-        if (startDate && new Date(date) < new Date(startDate as string)) return false
-        if (endDate && new Date(date) > new Date(endDate as string)) return false
-        if (filterPlace && place !== filterPlace) return false
-        if (filterType && type !== filterType) return false
-        if (filterSpender && spender !== filterSpender) return false
+        if (baslangicTarihi && new Date(tarih) < new Date(baslangicTarihi as string)) return false
+        if (bitisTarihi && new Date(tarih) > new Date(bitisTarihi as string)) return false
+        if (filterHarcamaYeri && harcamaYeri !== filterHarcamaYeri) return false
+        if (filterHarcamaTuru && harcamaTuru !== filterHarcamaTuru) return false
+        if (filterHarcamayiYapan && harcamayiYapan !== filterHarcamayiYapan) return false
 
         return true
       })
 
       res.status(200).json([headers, ...filteredData])
     } catch (error) {
-      console.error('Error fetching expenses:', error)
-      res.status(500).json({ message: 'Error fetching expenses', error })
+      console.error('Giderler alınırken hata oluştu:', error)
+      res.status(500).json({ message: 'Giderler alınırken hata oluştu', error })
     }
   } else {
     res.setHeader('Allow', ['GET'])
