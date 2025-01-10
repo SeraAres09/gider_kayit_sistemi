ece92b18ff36447de9c6dfdf1400e625e3a681c5
SeraAres09 <seraares202009@gmail.com>
2025-01-10 11:40:34 +0300

    Update addExpense.ts

--- pages/api/addExpense.ts
+++ pages/api/addExpense.ts
@@ -1,15 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { appendToSheet } from '../../utils/googleSheets'
+import { appendToExpensesSheet } from '../../utils/googleSheets'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
-      const { date, place, type, amount, spender, note } = req.body
-      const values = [[date, place, type, amount, spender, note]]
-      const result = await appendToSheet(values)
-      res.status(200).json({ message: 'Expense added successfully', result })
+      const { tarih, harcamaYeri, harcamaTuru, tutar, harcamayiYapan, not } = req.body
+      const values = [[tarih, harcamaYeri, harcamaTuru, tutar, harcamayiYapan, not]]
+      const result = await appendToExpensesSheet(values)
+      res.status(200).json({ message: 'Gider başarıyla eklendi', result })
     } catch (error) {
-      res.status(500).json({ message: 'Error adding expense', error })
+      res.status(500).json({ message: 'Gider eklenirken hata oluştu', error })
     }
   } else {
     res.setHeader('Allow', ['POST'])
