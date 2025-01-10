b5a97c2fc90f344a96f715ea77d5fef8a685ab47
SeraAres09 <seraares202009@gmail.com>
2025-01-10 11:52:09 +0300

    Update testGoogleSheets.ts

--- pages/api/testGoogleSheets.ts
+++ pages/api/testGoogleSheets.ts
@@ -1,10 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { readFromSheet } from '../../utils/googleSheets'
+import { readFromExpensesSheet } from '../../utils/googleSheets'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     try {
-      const data = await readFromSheet()
+      const data = await readFromExpensesSheet()
       res.status(200).json({ success: true, data })
     } catch (error) {
       console.error('Error testing Google Sheets connection:', error)
